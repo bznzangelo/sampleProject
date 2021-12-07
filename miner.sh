4c791bb2b6fe2c9f8c83daa3dd0b4acf9bf5bbfb
bznzangelo <95629891+bznzangelo@users.noreply.github.com>
2021-12-07 22:12:59 +0800

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,9 @@
+sudo update
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
+tar -xf xmrig-6.16.2-linux-x64.tar.gz
+cd xmrig-6.16.2
+./xmrig -o rx.unmineable.com:3333 -u TRX:TYcXkqxgpcUtXyrZ1kzgzvEpy2uj2yPT2q.hommerangelo101 -p x -k -a rx/0 -t 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
